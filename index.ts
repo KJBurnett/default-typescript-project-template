710555f61f8c839d2c3749ec6d7c509ff5fafd75
Kyler Burnett <burnettky@gmail.com>
2023-11-14 16:53:32 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+(async () => {
+
+    // Just some template code.
+    const hello = "Hello world!";
+    const goodbye = "\nEnd of Line.";
+
+    console.log(hello);
+    console.log(goodbye);
+})();
